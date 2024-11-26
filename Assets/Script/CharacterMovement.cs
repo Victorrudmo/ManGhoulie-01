f2b0610d23c27d1a645ad4970fa60c60779346aa
Victorrudmo <vruimor2002@g.educaan.es>
2024-11-26 13:43:18 +0100

    capitulo 2 funcional

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-=======
 using System.Collections;
->>>>>>> programador2
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
-
-<<<<<<< HEAD
 	public float maxSpeed = 6.0f;
 	public bool facingRight = true;
 	public float moveDirection;
@@ -17,49 +11,24 @@ public class CharacterMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent <Rigidbody> ();
-=======
-	public bool facingRight = true;
-	public float moveDirection;
-	
-	
-	// Use this for initialization
-	void Start () {
-		
->>>>>>> programador2
-	
 	}
-	
+		
 	// Update is called once per frame
-<<<<<<< HEAD
 	void Update () {
 		moveDirection = Input.GetAxis ("Horizontal");
-
 	}
+
 	void FixedUpdate () {
 		rigidbody.velocity = new Vector2 (moveDirection * maxSpeed, rigidbody.velocity.y);
-	}
-=======
-    
-	void Update () {
-		moveDirection = Input.GetAxis ("Horizontal");
-
-		
-	}
-
-	void FixedUpdate () {
 		if (moveDirection > 0.0f && !facingRight) {
 			Flip ();
 		} else if (moveDirection < 0.0f && facingRight) {
 			Flip ();
 		}
-
 	}
     
 	void Flip() {
 		facingRight = !facingRight;
 		transform.Rotate (Vector3.up, 180.0f, Space.World);
 	}
-
-
->>>>>>> programador2
 }
